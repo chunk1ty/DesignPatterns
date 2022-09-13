d18cb12d8dc99a1ba49e50d370f93e9d1e6211ff
chunk1ty <andriyan.krastev@gmail.com>
2022-09-14 02:01:50 +0300

    Added Abstract factory

---
+++ src/Creational/AbstractFactory/IDiscountService.cs
@@ -0,0 +1,31 @@
+﻿namespace AbstractFactory;
+
+/// <summary>
+///  AbstractProduct
+/// </summary>
+public interface IDiscountService
+{
+    public int Discount();
+}
+
+/// <summary>
+/// ConcreteProduct
+/// </summary>
+public class BulgarianDiscountService : IDiscountService
+{
+    public int Discount()
+    {
+        return 14;
+    }
+}
+
+/// <summary>
+/// ConcreteProduct
+/// </summary>
+public class AmericanDiscountService : IDiscountService
+{
+    public int Discount()
+    {
+        return 10;
+    }
+}
---
+++ src/Creational/AbstractFactory/Program.cs
@@ -0,0 +1,7 @@
+﻿using AbstractFactory;
+
+var shoppingCartForBulgaria = new ShoppingCart(new BulgarianShoppingCartPurchaseFactory());
+shoppingCartForBulgaria.CalculateCosts();
+
+var shoppingCartForAmerica = new ShoppingCart(new AmericanShoppingCartPurchaseFactory());
+shoppingCartForAmerica.CalculateCosts();
