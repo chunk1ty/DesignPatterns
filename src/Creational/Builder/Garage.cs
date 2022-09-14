b5050bdb4907b53b89111e1933a8dfe75cf03a04
andriyankrastev-klips <andriyank@klips.com>
2022-09-14 14:45:36 +0300

    Added Builder

---
+++ src/Creational/Builder/Garage.cs
@@ -0,0 +1,27 @@
+﻿namespace Builder;
+
+/// <summary>
+/// Director
+/// </summary>
+public class Garage
+{
+    private CarBuilder _builder;
+
+    public Garage()
+    {
+    }
+
+    public void Construct(CarBuilder builder)
+    {
+        _builder = builder;
+
+        _builder.BuildEngine();
+        _builder.BuildFrame();
+    }
+
+    // variation: the show method on the director instead of on the product.
+    public void Show()
+    {
+        Console.WriteLine(_builder.Car.ToString());
+    }
+}
---
+++ src/Creational/Builder/Program.cs
@@ -0,0 +1,14 @@
+﻿using Builder;
+
+var garage = new Garage();
+
+CarBuilder vwBuilder = new VwBuilder();
+CarBuilder bmwBuilder = new BMWBuilder();
+
+garage.Construct(vwBuilder);
+garage.Show();
+
+garage.Construct(bmwBuilder);
+garage.Show();
+
+Console.ReadKey();
