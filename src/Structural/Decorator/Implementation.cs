6284f0bc78379f4d56f9fda92acc7bae829a625d
chunk1ty <andriyan.krastev@gmail.com>
2022-09-14 23:47:04 +0300

    Added Decorator

---
+++ src/Structural/Decorator/Implementation.cs
@@ -0,0 +1,57 @@
+﻿namespace Decorator;
+
+/// <summary>
+/// Component (as interface)
+/// </summary>
+public interface IElectricalDevice
+{
+    void ConsumeElectricity(double electricity);
+}
+
+/// <summary>
+/// ConcreteComponent1
+/// </summary>
+public class Laptop : IElectricalDevice
+{
+    private const double MaxCapacity = 100;
+
+    public double ElectricityCapacity { get; private set; }
+
+    public void ConsumeElectricity(double electricity)
+    {
+        ElectricityCapacity = Math.Min(this.ElectricityCapacity + electricity, MaxCapacity);
+    }
+
+    public override string ToString()
+    {
+        return $"Laptop with capacity: {(this.ElectricityCapacity / MaxCapacity) * 100}%";
+    }
+}
+
+/// <summary>
+/// Decorator (as abstract base class)
+/// </summary>
+public class HighElectricityDefenderDecorator : IElectricalDevice
+{
+    private const double MaxElectricityAllowed = 100;
+
+    private readonly IElectricalDevice _electricalDevice;
+
+    public HighElectricityDefenderDecorator(IElectricalDevice electricalDevice)
+    {
+        _electricalDevice = electricalDevice;
+    }
+
+    public void ConsumeElectricity(double electricity)
+    {
+        if (electricity <= MaxElectricityAllowed)
+        {
+            _electricalDevice.ConsumeElectricity(electricity);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Defender with:\n{_electricalDevice}";
+    }
+}
---
+++ src/Structural/Decorator/Program.cs
@@ -0,0 +1,8 @@
+﻿using Decorator;
+
+IElectricalDevice laptop = new Laptop();
+var defender = new HighElectricityDefenderDecorator(laptop);
+
+defender.ConsumeElectricity(50);
+
+Console.WriteLine(laptop);
