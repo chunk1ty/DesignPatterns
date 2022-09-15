bc65c6ee9ede51ba4d856f3ae3ee5d9234cf46e8
chunk1ty <andriyan.krastev@gmail.com>
2022-09-15 23:09:09 +0300

    Added Composite

---
+++ src/Structural/Composite/Implementation.cs
@@ -0,0 +1,56 @@
+﻿namespace Decorator;
+
+/// <summary>
+/// Component
+/// </summary>
+public interface IElectricalDevice
+{
+    void ConsumeElectricity(double electricity);
+}
+
+/// <summary>
+/// Leaf
+/// </summary>
+public class Laptop : IElectricalDevice
+{
+    private const double MaxCapacity = 100;
+
+    public double ElectricityCapacity { get; private set; }
+
+    public void ConsumeElectricity(double electricity)
+    {
+        ElectricityCapacity = Math.Min(this.ElectricityCapacity + electricity, MaxCapacity);
+    }
+
+    public override string ToString()
+    {
+        return $"Laptop with capacity: {(this.ElectricityCapacity / MaxCapacity) * 100}%";
+    }
+}
+
+/// <summary>
+/// Composite
+/// </summary>
+public class PowerStrip : IElectricalDevice
+{
+    private readonly IEnumerable<IElectricalDevice> _electricalDevices;
+
+    public PowerStrip(IEnumerable<IElectricalDevice> electricalDevices)
+    {
+        this._electricalDevices = electricalDevices;
+    }
+
+    public void ConsumeElectricity(double electricity)
+    {
+        foreach (IElectricalDevice consumer in this._electricalDevices)
+        {
+            consumer.ConsumeElectricity(electricity);
+            Console.WriteLine(consumer);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Power strip with devices:\n{string.Join("\n", this._electricalDevices)}";
+    }
+}
---
+++ src/Structural/Composite/Program.cs
@@ -0,0 +1,6 @@
+﻿using Decorator;
+
+var laptop = new Laptop();
+var powerStrip = new PowerStrip(new List<IElectricalDevice>() {laptop});
+
+powerStrip.ConsumeElectricity(100);
