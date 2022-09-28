4a77e22fed6919f73c3c389e236665d6f9f42d2f
andriyankrastev-klips <andriyank@klips.com>
2022-09-28 09:58:42 +0300

    Added Flyweight

---
+++ src/Structural/Flyweight/Implementation.cs
@@ -0,0 +1,106 @@
+﻿namespace Flyweight;
+
+/// <summary>
+/// Flyweight
+/// </summary>
+public interface ICharacter
+{
+    void Draw(string fontFamily, int fontSize);
+}
+
+/// <summary>
+/// Concrete Flyweight
+/// </summary>
+public class CharacterA : ICharacter
+{
+    // intrinsic state - other objects can only read it, not change it.
+    private char _actualCharacter = 'a';
+    
+    // extrinsic state - altered “from the outside” by other objects
+    private string _fontFamily = string.Empty;
+    private int _fontSize;
+
+    public void Draw(string fontFamily, int fontSize)
+    {
+        _fontFamily = fontFamily;
+        _fontSize = fontSize;
+        Console.WriteLine($"Drawing {_actualCharacter}, {_fontFamily} {_fontSize}");
+    }
+}
+
+/// <summary>
+/// Concrete Flyweight
+/// </summary>
+public class CharacterB : ICharacter
+{
+    private char _actualCharacter = 'b';
+    private string _fontFamily = string.Empty;
+    private int _fontSize;
+
+    public void Draw(string fontFamily, int fontSize)
+    {
+        _fontFamily = fontFamily;
+        _fontSize = fontSize;
+        Console.WriteLine($"Drawing {_actualCharacter}, {_fontFamily} {_fontSize}");
+    }
+}
+
+/// <summary>
+/// FlyweightFactory
+/// </summary>
+public class CharacterFactory
+{
+    private readonly Dictionary<char, ICharacter> _characters = new();
+
+    public ICharacter? GetCharacter(char characterIdentifier)
+    {
+        if (_characters.ContainsKey(characterIdentifier))
+        {
+            Console.WriteLine("Character reuse");
+            return _characters[characterIdentifier];
+        }
+
+        Console.WriteLine("Character construction");
+        switch (characterIdentifier)
+        {
+            case 'a':
+                _characters[characterIdentifier] = new CharacterA();
+                return _characters[characterIdentifier];
+            case 'b':
+                _characters[characterIdentifier] = new CharacterB();
+                return _characters[characterIdentifier];
+            // and so on...               
+        }
+
+        return null;
+    }
+    
+    public ICharacter CreateParagraph(List<ICharacter> characters, int location)
+    {
+        return new Paragraph(characters, location);
+    }
+}
+
+/// <summary>
+/// Unshared Concrete Flyweight
+/// </summary>
+public class Paragraph : ICharacter
+{
+    private int _location;
+    private List<ICharacter> _characters = new();
+
+    public Paragraph(List<ICharacter> characters, int location)
+    {
+        _characters = characters;
+        _location = location;
+    }
+
+    public void Draw(string fontFamily, int fontSize)
+    {
+        Console.WriteLine($"Drawing in paragraph at location {_location}");
+        foreach (var character in _characters)
+        {
+            character.Draw(fontFamily, fontSize);
+        }
+    }
+}
